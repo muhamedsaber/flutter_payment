e7aaf5f194f9d67fbeec354927d5cbe27d878c37
saber son <elmohandesstamera@gmail.com>
2024-09-19 04:19:59 +0300

    Initial commit

---
+++ lib/flutter_payment_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FlutterPaymentApp extends StatelessWidget {
+  const FlutterPaymentApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+import 'flutter_payment_app.dart';
+
+void main() {
+  runApp(const FlutterPaymentApp());
+}
+
